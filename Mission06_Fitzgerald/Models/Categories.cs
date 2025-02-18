f490a6067bf6a1447cfde1b753e75333d9927530
fitzkai <fitzkaitlyn12@gmail.com>
2025-02-18 13:53:15 -0700

    update models/database

---
+++ Mission06_Fitzgerald/Models/Categories.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission06_Fitzgerald.Models
+{
+    public class Categories
+    {
+        [Required]
+        [Key]
+        public int CategoryID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+
+    }
+}
--- Mission06_Fitzgerald/Models/Movie.cs
+++ Mission06_Fitzgerald/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace Mission06_Fitzgerald.Models
@@ -7,18 +8,24 @@ namespace Mission06_Fitzgerald.Models
     {
         [Required]
         public int MovieID { get; set; }
-        [Required]
-        public string Category { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public int? CategoryID { get; set; }
+        public Categories? Category { get; set; }
+
         [Required]
         public string MovieTitle { get; set; }
         [Required]
         public int Year { get; set; }
         [Required]
-        public string Director { get; set; }
+        public string? Director { get; set; }
+        public string? Rating { get; set; }
         [Required]
-        public string Rating { get; set; }
-        public bool? Edited { get; set; }
-        public string? LentTo { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        [Required]
+        public bool CopiedToPlex { get; set; }
+
         [MaxLength(25)]
         public string? Notes { get; set; }
 
